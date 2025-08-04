2a5e0d337151dbed4aa5c12ff231ab90a5ae3f61
Sifan23 <sifanyg123@gmail.com>
2025-08-04 15:42:48 +0300

    rafactoring: update on product section import

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
-import ProductsSection from "@/components/sections/ProductsSection";
+import ProductsSection from "@/components/sections/product/ProductsSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
 import ContactSection from "@/components/sections/ContactSection";
-{/*import OrderForm from "@/components/sections/OrderForm";*/}
+{
+  /*import OrderForm from "@/components/sections/OrderForm";*/
+}
 
 export default function HomePage() {
   return (
